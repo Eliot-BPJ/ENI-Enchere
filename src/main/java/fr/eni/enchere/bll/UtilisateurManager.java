c448b5d35102e7488fd5bd2a65c89d9a6bc19435
Eliot Boyer <52098291+Eliot-BPJ@users.noreply.github.com>
2023-04-06 15:11:58 +0200

    ta mere 3

--- src/main/java/fr/eni/enchere/bll/UtilisateurManager.java
+++ src/main/java/fr/eni/enchere/bll/UtilisateurManager.java
@@ -1,5 +1,7 @@
 package fr.eni.enchere.bll;
 
+import fr.eni.enchere.bo.UtilisateurBO;
+import fr.eni.enchere.dal.DALException;
 import fr.eni.enchere.dal.DAOFactory;
 import fr.eni.enchere.dal.UtilisateurDAO;
 
@@ -8,6 +10,15 @@ public class UtilisateurManager {
 
 	public String connectUtilisateur(String pseudo, String password) {
 //		this.userDAO.SeConnecter(user, password);
+		try {
+			UtilisateurBO user = this.userDAO.SeConnecter(pseudo, password);
+			if (user == null) return "ko";
+			System.out.println(user);
+			return "ok";
+			
+		} catch (DALException e) {
+			e.printStackTrace();
+		}
 		return "ko";
 	}
 }
